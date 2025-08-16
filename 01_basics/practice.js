ee6cbc183d5caaa3419ddf611ed9ded503804069
Prakash singh <prakashsingh17my@gmail.com>
2025-08-16 19:25:49 +0000

    practice

--- 01_basics/practice.js
+++ 01_basics/practice.js
@@ -11,12 +11,16 @@ else {
     
 }*/
 
-let num = [50,60,80,98,67];
+/*let num = [50,60,80,98,67];
 let largest = num[0]
 for(let i = 1; i<= num.length;i++){
     if(num[i]>largest){
         largest = num[i]
     }
 }
-console.log(`the largest number is ${largest}`);
+console.log(`the largest number is ${largest}`);*/
 
+
+let num = [50,60,80,98,67];
+num.shift();
+console.log(num);

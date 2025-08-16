08c3660b5b6680e7b3d248c582fdcb4b74abe112
Prakash singh <prakashsingh17my@gmail.com>
2025-08-16 19:12:18 +0000

    practice

---
+++ 01_basics/practice.js
@@ -0,0 +1,22 @@
+/*let a = "prakash"
+// let b = a.split("").reverse().join("");
+console.log(a.split("").reverse().join(""));*/
+
+/*let str = "abb"
+if(str == str.split("").reverse().join("")){
+    console.log("this string is palindrom");
+}
+else {
+    console.log("this string in not a plaindrom");
+    
+}*/
+
+let num = [50,60,80,98,67];
+let largest = num[0]
+for(let i = 1; i<= num.length;i++){
+    if(num[i]>largest){
+        largest = num[i]
+    }
+}
+console.log(`the largest number is ${largest}`);
+
